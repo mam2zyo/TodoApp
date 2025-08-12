f9ed22988d90547d5a46838e2bcc7de951fed036
nohojong <shghwhs123@gmail.com>
2025-08-12 17:07:54 +0900

    test3

--- todoback/src/main/java/net/codecraft/todoback/nohojong/Main.java
+++ todoback/src/main/java/net/codecraft/todoback/nohojong/Main.java
@@ -1,4 +1,7 @@
 package net.codecraft.todoback.nohojong;
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
 }
--- todoback/src/main/java/net/codecraft/todoback/nohojong/Service.java
+++ todoback/src/main/java/net/codecraft/todoback/nohojong/Service.java
@@ -1,4 +1,5 @@
 package net.codecraft.todoback.nohojong;
 
 public class Service {
+
 }
