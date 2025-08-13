2b5163043f70b0050fd67f6323f5837998acb34c
nohojong <shghwhs123@gmail.com>
2025-08-13 18:15:40 +0900

    login test

--- todoback/src/main/java/net/codecraft/todoback/nohojong/Main.java
+++ todoback/src/main/java/net/codecraft/todoback/nohojong/Main.java
@@ -1,7 +1,7 @@
 package net.codecraft.todoback.nohojong;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //로그인주석추가
         //test
     }
 }
